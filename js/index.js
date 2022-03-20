3d90841eaccdfc081331bb2d01c9c5e26479be93
alchemistOfWeb <nikitakzntcv@gmail.com>
2022-03-20 17:56:16 +0400

    added collapse-elements

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ import { tickets } from "./tickets.js";
 
 $(()=>{
     console.log(tickets);
-    let pagetitle = $('.page-title');
+    let pagetitle = $('#page-title');
     console.log(pagetitle);
 });
