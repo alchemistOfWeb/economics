c88037a6f821c98101a923147fa9000ea4c8fa19
alchemistOfWeb <nikitakzntcv@gmail.com>
2022-06-07 06:52:03 +0400

    added other 20 questions

--- js/tickets.js
+++ js/tickets.js
@@ -190,9 +190,208 @@ export const macro = [
         sources: [],
     },
     {
-        title: "Безработица: формы, виды и места обитания",
+        title: "Безработица: формы, виды",
         number: "18",
         html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Безработица: регулирование, меры борьбы",
+        number: "19",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Определение уровня безработицы",
+        number: "20",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Сущность инфляции",
+        number: "21",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Уровень и темп инфляции",
+        number: "22",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Виды инфляции",
+        number: "23",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Типы и формы инфляции",
+        number: "24",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Инфляция и устойчивость денежного обращения",
+        number: "25",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Сущность и цели кредитно-денежной политики",
+        number: "26",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Функции центрального банка",
+        number: "27",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Инструменты кредитно-денежной политики государства",
+        number: "28",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "",
+        number: "29",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Денежная масса",
+        number: "30",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Государственный бюджет и его структура",
+        number: "31",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Налоговая система",
+        number: "32",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Фискальная политика",
+        number: "33",
+        html: `
+            государственная политика, направленная на регулирование экономики с помощью правительственных расходов и налогов.
+        `,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Дефляция",
+        number: "34",
+        html: ``,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Стагфляция",
+        number: "35",
+        html: `
+            инфляция, сопровождающаяся ростом безработицы.
+        `,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Бюджетный дефицит",
+        number: "36",
+        html: `
+            превышение государственных расходов над государственными доходами.
+        `,
+        lecture: {
+            title: "",
+            href: ""
+        },
+        sources: [],
+    },
+    {
+        title: "Государственный долг внешний (внутр./внешн.)",
+        number: "37",
+        html: `
+            <strong>Государственный долг внешний</strong> - задолженность государства перед субъектами другой страны или перед странами.
+            <strong>Государственный долг внутренний</strong> - задолженность государства перед гражданами, фирмами и учреждениями данной страны.
+        `,
         lecture: {
             title: "",
             href: ""
